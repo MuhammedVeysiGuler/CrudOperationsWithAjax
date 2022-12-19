cee109d2f9f5fd2df12adc42ef31bf6fb50a8394
MuhammedVeysiGuler <m.veysiguler@gmail.com>
2022-12-20 00:52:47 +0300

    html2pdf js eklendi

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/fetch', [SignInController::class, 'fetch'])->name('sign_in.fetch');
 Route::post('/delete', [SignInController::class, 'delete'])->name('sign_in.delete');
 Route::get('/get', [SignInController::class, 'get'])->name('sign_in.get');
 Route::post('/update', [SignInController::class, 'update'])->name('sign_in.update');
+
+Route::post('/pdf', [SignInController::class, 'pdf'])->name('pdf');
